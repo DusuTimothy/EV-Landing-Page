36cd668d0b7b9985a82a4c74ac4afa50c40304b7
Dusu Timothy <dususunday@gmail.com>
2026-08-28 17:22:17 +0100

    add About us component

--- Components/AboutUs.jsx
+++ Components/AboutUs.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 
 const AboutUs = () => {
-  return (
-    <div>
-        <div></div>
+    return (
         <div>
-            <h3>ABOUT US</p>
-            <h2>Driving Africa’s Electric Future</h2>
-            <p>At RoadRunner EV, we’re on a mission to revolutionize mobility through clean, smart, and accessible electric transportation. From eco-friendly tricycles to innovative battery swap services and expanding EV infrastructure, we provide solutions that power businesses and individuals alike. Built for performance and sustainability, our vehicles are designed to meet the unique challenges of urban and rural transportation — all while reducing carbon emissions and fueling progress.</p>
-            
+            <div>
+
+            </div>
+            <div className=''>
+                <h4 className='text-[#F4842B]'>ABOUT US</h4>
+                <h2 className='text-[#042C5B] text-[36px]'>Driving Africa’s Electric Future</h2>
+                <p className='text-[#797979]'> At RoadRunner EV, we’re on a mission to revolutionize mobility through clean, smart, and accessible electric transportation. From eco-friendly tricycles to innovative battery swap services and expanding EV infrastructure, we provide solutions that power businesses and individuals alike. Built for performance and sustainability, our vehicles are designed to meet the unique challenges of urban and rural transportation — all while reducing carbon emissions and fueling progress.</p>
+                <div>
+                    <h3 className='text-[#042C5B] text-[36px]'>Why RoadRunner?</h3>
+                    <p className='text-[#797979]'>
+                        ✔ Affordable, high-performing EVs <br />
+                        ✔ Convenient battery swap and charging networks <br />
+                        ✔ Trusted by riders, businesses, and delivery services <br />
+                        ✔ Committed to a greener, smarter tomorrows. <br />
+                    </p>
+
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export default AboutUs
+export default AboutUs;
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from '../Components/Nav'
 import Hero from '../Components/Hero'
+import AboutUs from '../Components/AboutUs'
 
 const App = () => {
   return (
     <div>
       <Nav />
       <Hero />
+      <AboutUs />
         
     </div>
   )
