c9c6f145a938e5e6ad99a49aded481bb3077623a
Dusu Timothy <dususunday@gmail.com>
2026-08-28 17:02:31 +0100

    add About us component

---
+++ Components/AboutUs.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>
+        <div></div>
+        <div>
+            <h3>ABOUT US</p>
+            <h2>Driving Africa’s Electric Future</h2>
+            <p>At RoadRunner EV, we’re on a mission to revolutionize mobility through clean, smart, and accessible electric transportation. From eco-friendly tricycles to innovative battery swap services and expanding EV infrastructure, we provide solutions that power businesses and individuals alike. Built for performance and sustainability, our vehicles are designed to meet the unique challenges of urban and rural transportation — all while reducing carbon emissions and fueling progress.</p>
+            
+        </div>
+    </div>
+  )
+}
+
+export default AboutUs
